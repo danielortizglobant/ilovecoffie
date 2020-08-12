d16a43ebf8b2cc2fb58ff5465c45ca1f597941c8
daniel ortiz <daniel.ortiz@globant.com>
2020-08-11 19:34:17 -0500

    Se agregan query params  y post para entender el funcionamiento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    CoffeesController, AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
